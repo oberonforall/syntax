ee76bff3343847db9efd73ec1fac110ea27ee8a9
amtoine <44101798+AntoineStevan@users.noreply.github.com>
2022-10-03 15:53:09 +0200

    add variables and echoes to the check script

--- check.sh
+++ check.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-# generate the files
-nu syntax.nu > .syntax.json
-python syntax.py --all --path .syntax.json > .generated.txt
+rule_file="rules.txt"
+syntax_file=".syntax.json"
+reconstruction_file=".generated.txt"
 
-vimdiff rules.txt .generated.txt
+# generate the files
+nu syntax.nu > "$syntax_file"
+echo "Syntax file generated at '$syntax_file'"
+
+python syntax.py --all --path "$syntax_file" > "$reconstruction_file"
+echo "Reconstructed file generated at '$reconstruction_file'"
+
+vimdiff "$rule_file" "$reconstruction_file"
