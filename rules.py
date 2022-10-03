271c68e91f5963ed96ed1fac1b5bc55f3626350c
amtoine <44101798+AntoineStevan@users.noreply.github.com>
2022-10-03 15:53:09 +0200

    add the skeleton to generate one rule by name

--- rules.py
+++ rules.py
@@ -1,16 +1,23 @@
+from typing import Any, Dict
+
 import json
 from pprint import pprint
 
-
 RULES_PATH = "rules.json"
+RULE_NAME = "letter"
 
 
-def main():
-    with open(RULES_PATH, "r") as rules_file:
+def generate_rule(name: str, *, rules: Dict[str, Any]) -> str:
+    return "TODO"
+
+
+def main(*, rules_path: str, rule_name: str):
+    with open(rules_path, "r") as rules_file:
         rules = json.load(rules_file)
 
-    pprint(rules)
+    rule = generate_rule(rule_name, rules=rules)
+    print(f"'{RULE_NAME}' = '{rule}'")
 
 
 if __name__ == "__main__":
-    main()
+    main(rules_path=RULES_PATH, rule_name=RULE_NAME)
