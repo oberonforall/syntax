9acc351ce23faf9cf86b6cb1480a2f1946296b0d
amtoine <44101798+AntoineStevan@users.noreply.github.com>
2022-10-04 10:11:15 +0200

    move the scripts to ./scripts and fix the paths

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-rule_file="rules.txt"
+rule_file="./res/rules.txt"
 syntax_file=".syntax.json"
 reconstruction_file=".generated.txt"
 
 # generate the files
-nu syntax.nu > "$syntax_file"
+./scripts/syntax.nu > "$syntax_file"
 echo "Syntax file generated at '$syntax_file'"
 
-python syntax.py --all --path "$syntax_file" > "$reconstruction_file"
+python ./scripts/syntax.py --all --path "$syntax_file" > "$reconstruction_file"
 echo "Reconstructed file generated at '$reconstruction_file'"
 
 vimdiff "$rule_file" "$reconstruction_file"
