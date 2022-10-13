5dd0dc15afd669ac0843a926c330b17d5e67ed44
amtoine <44101798+amtoine@users.noreply.github.com>
2022-10-13 19:30:06 +0200

    remove the ";" a the end of the signatures

--- scripts/website.py
+++ scripts/website.py
@@ -176,12 +176,12 @@ def generate_builtins(website_path: str, template_path: str):
                 if len(types) == 1:
                     in_type = types
                     signatures = [
-                        f"{name}({token});" for token in in_type[0].split("\\n")
+                        f"{name}({token})" for token in in_type[0].split("\\n")
                     ]
                 elif len(types) == 2:
                     in_type, out_type = types
                     signatures = [
-                        f"{name}({in_token}): {out_token};" for in_token, out_token in zip(in_type.split("\\n"), [out_type])
+                        f"{name}({in_token}): {out_token}" for in_token, out_token in zip(in_type.split("\\n"), [out_type])
                     ]
                 else:
                     raise ValueError("Two many input/output types, should only be 2.")
